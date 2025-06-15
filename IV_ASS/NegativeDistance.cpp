3be2776ed6920ad0c28718a9ea66e7286a0909cd
LorenzoDavalli <322893@studenti.unimore.it>
2025-06-15 16:38:37 +0200

    Update NegativeDistance.cpp
    aggiornamento esempio con negative distance .cpp

--- IV_ASS/NegativeDistance.cpp
+++ IV_ASS/NegativeDistance.cpp
@@ -1,15 +1,15 @@
 void fun(){
 
         int v[20];
+        int v2[20];
 
         for (int i = 0; i < 15; i++){
                 v[i] = 1;
 
         }
-        //Verificare che hanno lo stesso rray di base
 
 
         for (int i = 0; i < 15; i++){
-                v[i] = v[i+5];
+                v2[i] = v[i+2] + 1;
         }
 }
